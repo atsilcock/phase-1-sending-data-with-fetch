fee77eeb878a54d08db5a20b9f61e431e78df797
Alin Silcock <alinsilcock@Alin-MacBook-Air.local>
2023-11-30 17:34:58 -0700

    sending data

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Add your code here
+function submitData(){
+return fetch("http://localhost:3000/users", {
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json",
+    "Accept": "application/json",
+  },
+  body: JSON.stringify({
+    name: "Steve",
+    email: "steve@steve.com"
+  }),
+})
+
+.then(response => response.json())
+.then(response => {
+    console.log(response.id)
+    document.body.append(response.id)
+})
+
+.catch(response => {
+    document.body.append(response)
+})
+
+}
